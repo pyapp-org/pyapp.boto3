fbd14d568a476a5538df9435e0734dab422fb239
Tim Savage <tim@savage.company>
2019-06-22 00:19:45 +1000

    Updated to work with latest extension design

--- pyapp_ext/boto3/factory.py
+++ pyapp_ext/boto3/factory.py
@@ -1,9 +1,9 @@
-import botocore
+import boto3
 
 from pyapp.conf.helpers import ThreadLocalNamedSingletonFactory
 
 
-class SessionFactory(ThreadLocalNamedSingletonFactory):
+class SessionFactory(ThreadLocalNamedSingletonFactory[boto3.Session]):
     """
     Factory for creating AWS sessions.
     """
@@ -13,27 +13,27 @@ class SessionFactory(ThreadLocalNamedSingletonFactory):
         "aws_access_key_id": None,
         "aws_secret_access_key": None,
         "aws_session_token": None,
+        "profile_name": None,
     }
 
-    def create_instance(self, name=None):
+    def create(self, name: str = None) -> boto3.Session:
         config = self.get(name)
-        session = botocore.get_session()
-
-        if config["region_name"]:
-            session.set_config_variable("region", config["region_name"])
-
-        if (
-            config["aws_access_key_id"]
-            or config["aws_secret_access_key"]
-            or config["aws_session_token"]
-        ):
-            session.set_credentials(
-                config["aws_access_key_id"],
-                config["aws_secret_access_key"],
-                config["aws_session_token"],
-            )
-
-        return session
+        return boto3.Session(**config)
 
 
 session_factory = SessionFactory("AWS")
+get_session = session_factory.create()
+
+
+def client(service_name: str, config_name: str = None):
+    """
+    Factory for creating AWS clients.
+    """
+    return get_session(config_name).client(service_name)
+
+
+def resource(service_name: str, config_name: str = None):
+    """
+    Factory for creating AWS resources
+    """
+    return get_session(config_name).resource(service_name)
